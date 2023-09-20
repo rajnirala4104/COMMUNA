bad8d735a701c6e63b46b7d413eee16964609d0c
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-09-20 19:30:24 +0530

    created models and started login page

--- frontend/src/pages/Chats.js
+++ frontend/src/pages/Chats.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, Suspense, useEffect, useState } from "react";
 import { getChats } from "../api/servces";
 
 export const Chats = () => {
    const [apiData, setApiData] = useState([]);
 
-   console.log(apiData);
    const fetchApiData = async () => {
       const data = await getChats();
       setApiData(data.data);
@@ -13,6 +12,25 @@ export const Chats = () => {
    useEffect(() => {
       fetchApiData();
    }, []);
+   console.log(apiData);
 
-   return <div>Chats</div>;
+   return (
+      <Fragment>
+         <Suspense fallback="loading...">
+            <div className="data">
+               {apiData.length &&
+                  apiData.map((singleData, i) => {
+                     return (
+                        <Fragment key={i}>
+                           <h2>
+                              <strong>{singleData.chatName} : </strong>
+                              {singleData.isGroupChat ? "Group" : "Person"}
+                           </h2>
+                        </Fragment>
+                     );
+                  })}
+            </div>
+         </Suspense>
+      </Fragment>
+   );
 };
