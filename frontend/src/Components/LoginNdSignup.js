77105a1f1a176e079a3f19788a40dc95614e239a
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-09-22 23:49:39 +0530

    show and hide password functionality

--- frontend/src/Components/LoginNdSignup.js
+++ frontend/src/Components/LoginNdSignup.js
@@ -4,6 +4,7 @@ export const LoginNdSignup = () => {
    const [login, setLogin] = useState(true);
    const [userLoginEmail, setUserLoginEmail] = useState(null);
    const [userLoginPassword, setUserLoginPassword] = useState(null);
+   const [showPassword, setShowPassword] = useState(false);
 
    return (
       <Fragment>
@@ -47,7 +48,7 @@ export const LoginNdSignup = () => {
                         </div>
                         <div className="relative flex h-10 w-full min-w-[200px] max-w-[24rem] my-4">
                            <input
-                              type="password"
+                              type={showPassword ? "text" : "password"}
                               className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 pr-20 font-sans text-sm font-normal text-gray-600 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                               placeholder=" "
                               onChange={(e) =>
@@ -59,9 +60,9 @@ export const LoginNdSignup = () => {
                               className="!absolute right-1 top-1 z-10 select-none rounded bg-blue-400 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none peer-placeholder-shown:pointer-events-none peer-placeholder-shown:bg-blue-gray-500 peer-placeholder-shown:opacity-50 peer-placeholder-shown:shadow-none"
                               type="button"
                               data-ripple-light="true"
-                              onClick={() => alert(userLoginPassword)}
+                              onClick={() => setShowPassword(!showPassword)}
                            >
-                              Show
+                              {!showPassword ? "Show" : "Hide"}
                            </button>
                            <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                               Password
