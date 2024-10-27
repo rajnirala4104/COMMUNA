26a90caf87d38fc01ebe1653b645e1ae2ce4e958
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-10-27 22:16:50 +0530

    chore: add some comments and lil bit changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,60 +2,71 @@ const User = require("../models/userModel");
 const generateToken = require("../configs/generateToken");
 const asyncHandler = require("express-async-handler");
 const { StatusCodes } = require("http-status-codes");
+const ApiError = require("../utils/apiError");
+const ApiResponse = require("../utils/apiResponse");
 
 const registieredUser = asyncHandler(async (req, res) => {
+   // 1. Get the data from req.body
    const { name, email, password, pic } = req.body;
 
+   // 2. check if all the fields are mandatory
    if (!name || !email || !password) {
-      res.status(400);
-      throw new Error("Please Enter all the Feilds");
+      throw new ApiError(StatusCodes.BAD_REQUEST, "All fields are mandatory");
    }
 
+   // 3. check if the user already exists
    const userExists = await User.findOne({ email });
+
    if (userExists) {
-      res.status(400);
-      throw new Error("User is alredy exists");
+      throw new ApiError(StatusCodes.BAD_REQUEST, "User Already Exists");
    }
 
-   const user = await User.create({ name, email, password, pic, });
+   // 4. create a new user
+   const user = await User.create({ name, email, password, pic });
 
-   if (user) {
-      res.status(200).json({
-         _id: user._id,
-         name: user.name,
-         email: user.email,
-         password: user.password,
-         pic: user.pic,
-         token: generateToken(user._id),
-      });
-   } else {
-      res.status(400);
-      throw new Error("Oops!! User not Found");
+   // 5. check if the user is created successfully
+   if (!user) {
+      throw new ApiError(StatusCodes.BAD_REQUEST, "Registration Failed");
    }
+
+   // 6. return the response with the user data
+   return res.status(StatusCodes.CREATED).json(
+      new ApiResponse(
+         StatusCodes.CREATED,
+         "Registration Successful",
+         {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            pic: user.pic,
+            token: generateToken(user._id),
+         }
+      )
+   );
 });
 
 const authUser = asyncHandler(async (req, res) => {
    const { email, password } = req.body;
    const user = await User.findOne({ email });
+   const isPasswordTrue = await user.matchPassword(password);
 
-   if (!user) {
-      res.status(StatusCodes.NOT_FOUND);
-      throw new Error("Invalid Email");
-   }
+   if (!user) throw new ApiError(StatusCodes.BAD_REQUEST, "Invalid Email");
+   if (!isPasswordTrue) throw new ApiError(StatusCodes.BAD_REQUEST, "Invalid Password");
 
-   if (await user.matchPassword(password)) {
-      res.json({
-         _id: user._id,
-         name: user.name,
-         email: user.email,
-         password: user.password,
-         pic: user.pic,
-         token: generateToken(user._id),
-      });
-   } else {
-      res.status(401);
-      throw new Error("Invalid Email or Password");
-   }
+   return res.status(StatusCodes.OK).json(
+      new ApiResponse(
+         StatusCodes.OK,
+         "Login Successful",
+         {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            pic: user.pic,
+            token: generateToken(user._id),
+         }
+      ));
 });
 
 // ------- /api/user?search=pulkit
